5e585c3878126d8c0c3304f5e57d7979fb264852
mingcute <bestdearlove@gmail.com>
2014-03-09 01:19:40 +0800

    - Complete google image search  - Add image resize in edit VC

---
+++ GoogleSearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  GoogleSearchViewController.h
+//  sticker
+//
+//  Created by 李健銘 on 2014/3/8.
+//  Copyright (c) 2014年 TakoBear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GoogleSearchViewController : UIViewController
+
+@end
---
+++ GoogleSearchViewController.m
@@ -0,0 +1,38 @@
+//
+//  GoogleSearchViewController.m
+//  sticker
+//
+//  Created by 李健銘 on 2014/3/8.
+//  Copyright (c) 2014年 TakoBear. All rights reserved.
+//
+
+#import "GoogleSearchViewController.h"
+
+@interface GoogleSearchViewController ()
+
+@end
+
+@implementation GoogleSearchViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
